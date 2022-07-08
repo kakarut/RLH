d6ab6cd2435dd2c4d2322a72af2c6358ed6a1ad9
gjwang <108599542+kakarut@users.noreply.github.com>
2022-07-08 16:07:50 +0800

    Update WN18RR.sh

--- configs/WN18RR.sh
+++ configs/WN18RR.sh
@@ -1,12 +1,16 @@
-#!/usr/bin/env bash
-
 data_input_dir="datasets/data_preprocessed/WN18RR/"
 vocab_dir="datasets/data_preprocessed/WN18RR/vocab"
+max_policy_high=4
+max_policy_low=2
+reward_gamma=1.2
 total_iterations=1000
 path_length=3
 hidden_size=50
-embedding_size=50
-batch_size=256
+embedding_size=100
+relation_size=100
+batch_size=100
+cluster=5
+regularization_lamda=0.005
 beta=0.05
 Lambda=0.05
 use_entity_embeddings=0
@@ -14,5 +18,5 @@ train_entity_embeddings=0
 train_relation_embeddings=1
 base_output_dir="output/WN18RR/"
 load_model=0
-model_load_dir="/home/sdhuliawala/logs/RL-Path-RNN/wn18rrr/edb6_3_0.05_10_0.05/model/model.ckpt"
+model_load_dir="/home/gjwang/RLH/wn18rr/log/rlhmodel/model.ckpt"
 nell_evaluation=0
