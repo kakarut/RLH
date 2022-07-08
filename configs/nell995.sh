e8c097689888a23afa350f5b987e6dfd3bb4cbb3
gjwang <108599542+kakarut@users.noreply.github.com>
2022-07-08 16:13:57 +0800

    Update nell995.sh

--- configs/nell995.sh
+++ configs/nell995.sh
@@ -1,12 +1,16 @@
-#!/usr/bin/env bash
-
-data_input_dir="datasets/data_preprocessed/nell/"
+data_input_dir="datasets/data_preprocessed/nell995/"
 vocab_dir="datasets/data_preprocessed/nell/vocab"
+max_policy_high=4
+max_policy_low=2
+reward_gamma=1.2
 total_iterations=3000
 path_length=3
 hidden_size=50
-embedding_size=50
-batch_size=128
+embedding_size=100
+relation_size=100
+batch_size=100
+cluster=65
+regularization_lamda=0.005
 beta=0.05
 Lambda=0.02
 use_entity_embeddings=0
@@ -14,6 +18,5 @@ train_entity_embeddings=0
 train_relation_embeddings=1
 base_output_dir="output/nell/worksfor"
 load_model=0
-model_load_dir="/home/sdhuliawala/logs/RL-Path-RNN/nnnn/45de_3_0.06_10_0.0/model/model.ckpt"
+model_load_dir="/home/gjwang/RLH/nell995/log/rlhmodel/model.ckpt"
 nell_evaluation=0
-
